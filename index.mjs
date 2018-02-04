7baf986745804c5b4c541d084a67d62ce8666547
Samuel Ytterbrink <neppord@gmail.com>
2018-02-04 15:10:43 +0100

    Add morgan to log http requests.

--- index.mjs
+++ index.mjs
@@ -1,7 +1,9 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
+app.use(morgan('tiny'));
 app.use(express.static('client'));
 
 app.listen(process.env.PORT || 8080);
