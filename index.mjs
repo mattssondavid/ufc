1b475f97025caf2a5babccdad2256c3b94c2b63a
Samuel Ytterbrink <neppord@gmail.com>
2018-02-04 15:07:58 +0100

    Add semicolons.

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import express from 'express';
 
-const app = express()
+const app = express();
 
-app.use(express.static('client'))
+app.use(express.static('client'));
 
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 8080);
