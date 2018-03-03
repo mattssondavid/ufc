2bb87a0179681494119364786e1afa6c22633244
Samuel Ytterbrink <neppord@gmail.com>
2018-03-03 16:38:08 +0100

    Implement basic gold mine state functions and refactor.
    Co-authored-by: David Mattsson <mattsson.david@gmail.com>

--- src/Model.mjs
+++ src/Model.mjs
@@ -1,10 +1,25 @@
-export const init = () => ({});
-export const setMoneyForPlayer = (amount, player) => state => {
-    let newState = {
-        [player]: amount
-    };
-    return Object.assign({}, state, newState);
+const setSubState = subStateName => subStateSetter => state => {
+    let newSubState = subStateSetter(state[subStateName]);
+    return Object.assign({}, state, {[subStateName] : newSubState});
 };
-export const getMoneyForPlayer = player => state => {
-    return state[player];
+const getSubState = subStateName => subStateGetter => state => {
+    return subStateGetter(state[subStateName]);
+};
+export const init = () => ({
+    goldmines: {},
+    money: {}
+});
+const setAmount = (amount, player) => state => ({...state, [player]: amount});
+const getAmount = player => state => state[player] || 0;
+export const setMoneyForPlayer = (amount, player) => {
+    return setSubState('money')(setAmount(amount, player));
+};
+export const getMoneyForPlayer = player => {
+    return getSubState('money')(getAmount(player));
+};
+export const getNumberOfGoldminesForPlayer = player => {
+    return getSubState('goldmines')(getAmount(player));
+};
+export const setNumberOfGoldminesForPlayer = (amount, player) => {
+    return setSubState('goldmines')(setAmount(amount, player));
 };
--- src/Model.test.mjs
+++ src/Model.test.mjs
@@ -1,17 +1,45 @@
 import {
     init,
     setMoneyForPlayer,
-    getMoneyForPlayer
+    getMoneyForPlayer,
+    getNumberOfGoldminesForPlayer,
+    setNumberOfGoldminesForPlayer
 } from './Model';
 import chai from 'chai';
 import tap from 'tap';
 const mocha = tap.mocha;
 const expect = chai.expect;
 
-mocha.describe('Init model', () => {
-    mocha.it('does not crash', () => {
+mocha.describe('Model', () => {
+    mocha.it('does not crash during creation', () => {
         init();
     });
+    mocha.it('contains both money and number of goldmines', () => {
+        {
+            let state1 = init();
+            let state2 = setMoneyForPlayer(1, 'John Doe')(state1);
+            let state3 = setNumberOfGoldminesForPlayer(2, 'John Doe')(state2);
+            let money = getMoneyForPlayer('John Doe')(state3);
+            expect(money).to.equal(1);
+        }
+        {
+            let state1 = init();
+            let state2 = setMoneyForPlayer(3, 'John Doe')(state1);
+            let state3 = setNumberOfGoldminesForPlayer(2, 'John Doe')(state2);
+            let money = getMoneyForPlayer('John Doe')(state3);
+            expect(money).to.equal(3);
+        }
+    });
+    /*mocha.it('can get amount of goldmines from player one and money from player two', () => {
+        let state1 = init();
+        let state2 = setMoneyForPlayer(1, 'John Doe')(state1);
+        let state3 = setMoneyForPlayer(2, 'Joanne Doe')(state2);
+        setNumberOfGoldminesForPlayer(3, 'John Doe')(state3);
+        let money = getMoneyForPlayer('Joanne Doe')(state3);
+        let mines = getNumberOfGoldminesForPlayer('John Doe')(state3);
+        expect(money).to.equal(2);
+        expect(mines).to.equal(3);
+    });*/
 });
 mocha.describe('moneyForPlayer', () => {
     mocha.it('finds money that got set for player with same name', () => {
@@ -50,5 +78,19 @@ mocha.describe('moneyForPlayer', () => {
         setMoneyForPlayer(300, 'John Doe')(state2);
         let money = getMoneyForPlayer('John Doe')(state2);
         expect(money).to.equal(200);
+    });
+});
+
+mocha.describe('goldminesForPlayer', () => {
+    mocha.it('finds the number of goldmines for a player', () => {
+        let state = init();
+        let numberOfGoldmines = getNumberOfGoldminesForPlayer('John Doe')(state);
+        expect(numberOfGoldmines).to.equal(0);
+    });
+    mocha.it('finds the number for of goldmines for player after it has been set', () => {
+        let state1 = init();
+        let state2 = setNumberOfGoldminesForPlayer(1, 'John Doe')(state1);
+        let numberOfMines = getNumberOfGoldminesForPlayer('John Doe')(state2);
+        expect(numberOfMines).to.equal(1);
     });
 });
