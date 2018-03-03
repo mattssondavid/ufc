87e6a215ed9c5d725f042cc92fee0a196e72bf0d
Samuel Ytterbrink <neppord@gmail.com>
2018-03-03 14:34:44 +0100

    Replace jest with tap and chai.

--- src/Model.test.js
+++ src/Model.test.js
@@ -2,48 +2,50 @@ const model = require('./Model');
 const init = model.init;
 const setMoneyForPlayer = model.setMoneyForPlayer;
 const getMoneyForPlayer = model.getMoneyForPlayer;
+const expect = require('chai').expect;
+const mocha = require('tap').mocha;
 
-describe('Model', function () {
-    it('should not crash', function () {
+mocha.describe('Model', function () {
+    mocha.it('should not crash', function () {
         init();
     });
-    it('should find money that got set for player with same name', () => {
+    mocha.it('should find money that got set for player with same name', () => {
         {
             let state1 = init();
             let state2 = setMoneyForPlayer(100, 'John Doe')(state1);
             let money = getMoneyForPlayer('John Doe')(state2);
-            expect(money).toBe(100);
+            expect(money).to.equal(100);
         }
         {
             let state1 = init();
             let state2 = setMoneyForPlayer(200, 'John Doe')(state1);
             let money = getMoneyForPlayer('John Doe')(state2);
-            expect(money).toBe(200);
+            expect(money).to.equal(200);
         }
     });
-    it('should find money for the correct player name', () => {
+    mocha.it('should find money for the correct player name', () => {
         {
             let state1 = init();
             let state2 = setMoneyForPlayer(200, 'John Doe')(state1);
             let state3 = setMoneyForPlayer(300, 'Joanne Doe')(state2);
             let money = getMoneyForPlayer('John Doe')(state3);
-            expect(money).toBe(200);
+            expect(money).to.equal(200);
         }
     });
-    it('should use the last value', function () {
+    mocha.it('should use the last value', function () {
 
         let state1 = init();
         let state2 = setMoneyForPlayer(200, 'John Doe')(state1);
         let state3 = setMoneyForPlayer(300, 'John Doe')(state2);
         let money = getMoneyForPlayer('John Doe')(state3);
-        expect(money).toBe(300);
+        expect(money).to.equal(300);
     });
-    it('should not modify old state', function () {
+    mocha.it('should not modify old state', function () {
 
         let state1 = init();
         let state2 = setMoneyForPlayer(200, 'John Doe')(state1);
         setMoneyForPlayer(300, 'John Doe')(state2);
         let money = getMoneyForPlayer('John Doe')(state2);
-        expect(money).toBe(200);
+        expect(money).to.equal(200);
     });
 });
