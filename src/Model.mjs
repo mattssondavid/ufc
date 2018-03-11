e4396c35ce56b8c8426461d8c8a7b881ab4d7276
Samuel Ytterbrink <neppord@gmail.com>
2018-03-11 14:41:48 +0100

    Change order of sub states, David was right.

--- src/Model.mjs
+++ src/Model.mjs
@@ -1,23 +1,34 @@
-const setSubState = (name, setter) => state => ({
+const modifySubState = name =>  setter => state => ({
     ...state,
     [name]: setter(state[name])
 });
-const getSubState = (name, getter) => state => getter(state[name]);
+const getSubState = name =>  getter => state => getter(state[name]);
+const modifyMoneyState = modifySubState('money');
+const getMoneyState = getSubState('money');
+const modifyGoldminesState = modifySubState('goldmines');
+const getGoldminesState = getSubState('goldmines');
 const defaultTo = def => a => a || def;
+const _const = c => v => c;
 
-export const init = () => ({
-    goldmines: {},
-    money: {}
-});
+export const init = () => ({});
 export const setMoneyForPlayer = (amount, player) => {
-    return setSubState('money', setSubState(player, () => amount));
-};
-export const getMoneyForPlayer = player => {
-    return getSubState('money', getSubState(player,  defaultTo(0)));
-};
-export const getNumberOfGoldminesForPlayer = player => {
-    return getSubState('goldmines', getSubState(player, defaultTo(0)));
+    return modifySubState(player)(
+        s => modifyMoneyState(_const(amount))(s || {})
+    );
 };
 export const setNumberOfGoldminesForPlayer = (amount, player) => {
-    return setSubState('goldmines', setSubState(player, () => amount));
+    return modifySubState(player)(
+        s => modifyGoldminesState(_const(amount))(s || {})
+    );
+};
+
+export const getMoneyForPlayer = player => {
+    return getSubState(player)(
+        s => getMoneyState(defaultTo(0))(s || {})
+    );
+};
+export const getNumberOfGoldminesForPlayer = player => {
+    return getSubState(player)(
+        s => getGoldminesState(defaultTo(0))(s || {})
+    );
 };
