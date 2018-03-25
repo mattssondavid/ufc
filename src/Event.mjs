b3511ae959e0c20e28e2749d4c72c3a56495d01a
Samuel Ytterbrink <neppord@gmail.com>
2018-03-25 13:44:58 +0200

    Added a empty event queue

--- src/Event.mjs
+++ src/Event.mjs
@@ -16,5 +16,8 @@ export let getState = er => eventResult(er.state, er.queue, er.state);
 export let putState = er => eventResult(er.value, er.queue, er.value);
 export let event = (time, action) => ({time: time, action: action});
 
+let compareEvents = (a, b) => Math.sign(a.time - b.time);
 export let eventQueue = e =>
-    new Heap([e], false, (a, b) => Math.sign(a.time - b.time));
+    new Heap([e], false, compareEvents);
+export let emptyEventQueue =
+    new Heap([], false, compareEvents);
