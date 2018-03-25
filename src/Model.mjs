b4f90dc6b4d9e9dcfd0b54c9b67842b6f5654d5b
Samuel Ytterbrink <neppord@gmail.com>
2018-03-25 13:13:42 +0200

    Remove braces from functions, and use multiple lines.

--- src/Model.mjs
+++ src/Model.mjs
@@ -1,8 +1,10 @@
-const modifySubState = name => setter => state => ({
-    ...state,
-    [name]: setter(state[name])
-});
-const getSubState = name => getter => state => getter(state[name]);
+const modifySubState =
+    name => setter => state => ({
+        ...state,
+        [name]: setter(state[name])
+    });
+const getSubState =
+    name => getter => state => getter(state[name]);
 const modifyMoneyState = modifySubState('money');
 const getMoneyState = getSubState('money');
 const modifyGoldminesState = modifySubState('goldmines');
@@ -11,24 +13,20 @@ const defaultTo = _default => a => a || _default;
 const _const = constant => _ => constant;
 
 export const init = () => ({});
-export const setMoneyForPlayer = (amount, player) => {
-    return modifySubState(player)(
+export const setMoneyForPlayer =
+    (amount, player) => modifySubState(player)(
         s => modifyMoneyState(_const(amount))(s || {})
     );
-};
-export const setNumberOfGoldminesForPlayer = (amount, player) => {
-    return modifySubState(player)(
+export const setNumberOfGoldminesForPlayer =
+    (amount, player) => modifySubState(player)(
         s => modifyGoldminesState(_const(amount))(s || {})
     );
-};
 
-export const getMoneyForPlayer = player => {
-    return getSubState(player)(
+export const getMoneyForPlayer =
+    player => getSubState(player)(
         s => getMoneyState(defaultTo(0))(s || {})
     );
-};
-export const getNumberOfGoldminesForPlayer = player => {
-    return getSubState(player)(
+export const getNumberOfGoldminesForPlayer =
+    player => getSubState(player)(
         s => getGoldminesState(defaultTo(0))(s || {})
     );
-};
