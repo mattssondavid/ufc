9186cc7a96a3c34123235797819f1594fc53ce60
Samuel Ytterbrink <neppord@gmail.com>
2018-03-29 10:18:55 +0200

    Replace let with const, immutable is king.

--- src/Action.mjs
+++ src/Action.mjs
@@ -3,33 +3,33 @@ import {
     eventQueue
 } from "./Event";
 
-export let result =
+export const result =
     (state, queue, value) => ({
         state: state,
         queue: queue,
         value: value
     });
 
-export let pure =
+export const pure =
     value => state => result(
         state,
         emptyEventQueue,
         value
     );
-export let map =
+export const map =
     fun => action => state => {
-        let er = action(state);
+        const er = action(state);
         return result(
             er.state,
             er.queue,
             fun(er.value)
         );
     };
-export let flatMap =
+export const flatMap =
     (action, actionConstructor) => state => {
-        let ar = action(state);
-        let newAction = actionConstructor(ar.value);
-        let ar2 = newAction(ar.state);
+        const ar = action(state);
+        const newAction = actionConstructor(ar.value);
+        const ar2 = newAction(ar.state);
         return result(
             ar2.state,
             ar.queue.merge(ar2.queue),
@@ -37,38 +37,38 @@ export let flatMap =
         );
     };
 
-export let addEvent =
+export const addEvent =
     event => state => result(
         state,
         eventQueue(event),
         undefined
     );
 
-export let getState =
+export const getState =
     state => result(
         state,
         emptyEventQueue,
         state
     );
 
-export let putState =
+export const putState =
     value => () => result(
         value,
         emptyEventQueue,
         undefined
     );
 
-export let modifyState =
+export const modifyState =
     f => state => result(
         f(state),
         emptyEventQueue,
         undefined
     );
 
-export let doAction = gen => doHelper(gen())();
+export const doAction = gen => doHelper(gen())();
 
-let doHelper = iterator => value => {
-    let {value: action, done} = iterator.next(value);
+const doHelper = iterator => value => {
+    const {value: action, done} = iterator.next(value);
     if (done) {
         if (action) return action;
         else return pure(value);
